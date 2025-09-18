389350cc68ea06097b5d63573c581ad44339136f
Febry Aryo Riandhito <Febryryo21@gmail.com>
2025-09-18 12:53:05 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: 'https://serv-blog.vercel.app/sitemap.xml',
+        sitemap: 'https://serv-agency.vercel.app/sitemap.xml',
     }
 }
